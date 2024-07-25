9bc775b7199bc2d06a26c9deeafbd6555908cf63
Umair Asmat <134450459+UmairAsmat@users.noreply.github.com>
2024-07-25 11:29:27 +0500

    Create xstartup.sh

---
+++ Selenium/xstartup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export XKL_XMODMAP_DISABLE=1
+export XDG_CURRENT_DESKTOP="GNOME-Flashback:Unity"
+export XDG_MENU_PREFIX="gnome-flashback-"
+unset DBUS_SESSION_BUS_ADDRESS
+xrdb "$HOME/.Xresources"
+gnome-session &

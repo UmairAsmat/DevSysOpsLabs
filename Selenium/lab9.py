9c533a3fe2876f8feca07083a910e9f3f4ec5105
Umair Asmat <134450459+UmairAsmat@users.noreply.github.com>
2024-07-26 12:10:45 +0500

    Running Headless Selenium Tests on EC2

---
+++ Selenium/lab9.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+
+# Configure Chrome options for headless mode
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--no-sandbox')
+
+# Initialize the WebDriver with Chrome options
+driver = webdriver.Chrome()
+
+# Open a webpage and perform actions (e.g., capturing screenshots)
+driver.get("https://alnafi.com")
+driver.save_screenshot("headless_screenshot.png")
+
+# Close the browser
+driver.quit()
